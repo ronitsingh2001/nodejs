5f968f4da7aa56ae0a8b392acca88065169887d2
ronit-singh <ronit2001krish@gmail.com>
2023-05-28 12:42:05 +0530

    static content added

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const adminRouter = require('./routes/admin')
 const shopRouter = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/admin/', adminRouter)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 
